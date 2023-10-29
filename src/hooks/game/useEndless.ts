ad39e6398e74667e79c445acd012851caa22a443
Smashinfries <jus10luka@gmail.com>
2023-10-29 17:31:21 -0500

    redesign fetch system

--- src/hooks/game/useEndless.ts
+++ src/hooks/game/useEndless.ts
@@ -1,17 +1,18 @@
-import { useEffect, useState } from "react";
-import { getFakeImage, getRealImage } from "../../api/api";
-import { DanPost } from "../../api/types";
-import { AIBOORU_URL, DANBOORU_URL } from "../../../constants";
+import { useCallback, useEffect, useState } from 'react';
+import { getFakeImage, getRealImage } from '../../api/api';
+import { UnifiedBooruPost } from '../../api/types';
+import { processAIPosts, processGelPosts, shufflePosts } from '../../utils/arrays';
 
 export const useEndless = () => {
     const [loading, setLoading] = useState(true);
-    const [currentImage, setCurrentImage] = useState<DanPost>();
-    const [weblink, setWeblink] = useState<string>('');
-    const [answer, setAnswer] = useState<'ai'|'real'|'none'>();
+    const [shuffledImages, setShuffledImages] = useState<UnifiedBooruPost[]>([]);
+    const [currentImage, setCurrentImage] = useState<UnifiedBooruPost>();
+    const [answer, setAnswer] = useState<'ai' | 'real' | 'none'>();
     const [isCorrect, setIsCorrect] = useState<boolean>();
     const [isCompleted, setIsCompleted] = useState(false);
     const [correct, setCorrect] = useState(0);
     const [incorrect, setIncorrect] = useState(0);
+    const [page, setPage] = useState(0);
 
     const handleCorrect = () => {
         setCorrect(correct + 1);
@@ -21,32 +22,56 @@ export const useEndless = () => {
         setIncorrect(incorrect + 1);
     };
 
-    const getRandomImage = async () => {
-        const randomNum = Math.random();
-        setCurrentImage(undefined);
-        setIsCompleted(false);
-        setIsCorrect(undefined);
-        setLoading(true);
-        if (randomNum > 0.5) {
-            const results = await getFakeImage({limit:1, tags:'1girl order:random rating:g,s age:<6month'});
-            if (results) {
-                setAnswer('ai');
-                setCurrentImage(results[0]);
-                setWeblink(AIBOORU_URL + '/posts/' + results[0].id)
-                setLoading(false);
-            }
+    const getRandomImage = useCallback(async (initialPage?:number) => {
+        if (shuffledImages.length < 1) {
+            console.log('Fetching new images');
+            const results = await fetchImages(initialPage ?? page + 1);
+            const selectedImage = results[results.length - 1];
+            setPage(prev => prev + 1);
+            setCurrentImage(selectedImage);
+            setIsCompleted(false);
+            setIsCorrect(undefined);
+            setAnswer(selectedImage?.isAI ? 'ai' : 'real');
+            setShuffledImages((prev) => prev.filter((image) => image.weblink !== selectedImage?.weblink));
         } else {
-            const results = await getRealImage({limit:1, tags:'1girl order:random rating:g,s age:<6month'});
-            if (results) {
-                setAnswer('real');
-                setCurrentImage(results[0]);
-                setWeblink(DANBOORU_URL + '/posts/' + results[0].id)
-                setLoading(false);
-            }
+            const images = [...shuffledImages];
+            const selectedImage = images[images.length - 1];
+            setCurrentImage(selectedImage);
+            setIsCompleted(false);
+            setIsCorrect(undefined);
+            setAnswer(selectedImage?.isAI ? 'ai' : 'real');
+            setShuffledImages((prev) => prev.filter((image) => image.weblink !== selectedImage?.weblink));
+        }
+    }, [shuffledImages]);
+
+    const fetchImages = async (page = 0) => {
+        setLoading(true);
+        const aiResults = await getFakeImage({
+            limit: 50,
+            page: page,
+            tags: '-animated 1girl order:random rating:g age:<6month',
+        });
+        const realResults = await getRealImage({
+            limit: 50,
+            pid: page,
+            tags: '-ai_assisted -animated 1girl solo rating:g',
+        });
+
+        if (aiResults?.length > 0 && realResults?.length > 0) {
+            const shuffledResults = shufflePosts([
+                ...processAIPosts(aiResults),
+                ...processGelPosts(realResults),
+            ]);
+            setShuffledImages(shuffledResults);
+            setLoading(false);
+            return shuffledResults;
+        } else {
+            setLoading(false);
+            return [];
         }
     };
 
-    const onSelect = (choice:'ai'|'real'|'none') => {
+    const onSelect = (choice: 'ai' | 'real' | 'none') => {
         if (choice === answer) {
             handleCorrect();
             setIsCorrect(true);
@@ -60,11 +85,18 @@ export const useEndless = () => {
 
     useEffect(() => {
         if (!currentImage) {
-            getRandomImage();
+            getRandomImage(0);
         }
-        
-    },[]);
+    }, []);
 
-    return {currentImage, correct, incorrect, answer, onSelect, getRandomImage, isCorrect, isCompleted, weblink}
-
+    return {
+        currentImage,
+        correct,
+        incorrect,
+        answer,
+        onSelect,
+        getRandomImage,
+        isCorrect,
+        isCompleted,
+    };
 };
