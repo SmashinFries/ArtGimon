b37b29eebaa8f8f429734e4022234732edb8117c
Smashinfries <jus10luka@gmail.com>
2023-10-29 17:29:29 -0500

    refactor

--- src/app/settings/index.tsx
+++ src/app/settings/index.tsx
@@ -1,11 +1,7 @@
-import { View } from "react-native";
+import { View } from 'react-native';
 
 const SettingsPage = () => {
-    return(
-        <View>
-            
-        </View>
-    )
+    return <View></View>;
 };
 
 export default SettingsPage;
