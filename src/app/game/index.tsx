8d7274b3bfa3e55c03d23a2eeda0320b17136e3b
Smashinfries <jus10luka@gmail.com>
2023-10-29 17:29:18 -0500

    styling

--- src/app/game/index.tsx
+++ src/app/game/index.tsx
@@ -1,22 +1,25 @@
-import { View, useWindowDimensions } from "react-native";
-import { Text } from "react-native-paper";
-import { MenuButton, MenuIconButton } from "../../components/buttons";
-import { EndlessHeader } from "../../components/headers";
+import { View, useWindowDimensions } from 'react-native';
+import { Text } from 'react-native-paper';
+import { MenuButton, MenuIconButton } from '../../components/buttons';
+import { EndlessHeader } from '../../components/headers';
 
 const GamePage = () => {
-    const {width} = useWindowDimensions();
-    return(
+    const { width } = useWindowDimensions();
+    return (
         <>
             <EndlessHeader />
-            <View style={{flex:1, justifyContent:'center', alignItems:'center'}}>
+            <View style={{ flex: 1, justifyContent: 'flex-start', alignItems: 'center' }}>
                 <Text variant="displayLarge">Select Mode</Text>
-                <MenuIconButton title="Endless" route="/game/endless" fixedWidth={width-(width*.75)} />
-                <MenuIconButton title="Time Attack" disabled fixedWidth={width-(width*.75)} />
-                <MenuIconButton title="Sets" disabled fixedWidth={width-(width*.75)} />
+                <MenuIconButton
+                    title="Endless"
+                    route="/game/endless"
+                    fixedWidth={width - width * 0.75}
+                />
+                {/* <MenuIconButton title="Time Attack" disabled fixedWidth={width-(width*.75)} />
+                <MenuIconButton title="Sets" disabled fixedWidth={width-(width*.75)} /> */}
             </View>
         </>
-        
-    )
+    );
 };
 
 export default GamePage;
