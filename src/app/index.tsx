1c53d5a5dafd57bc8e054c5384e2bb0e54d542c3
Smashinfries <jus10luka@gmail.com>
2023-10-29 17:28:42 -0500

    add toasts

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,22 +1,27 @@
-import { BackHandler, Platform, View } from "react-native";
-import { Text } from "react-native-paper";
-import { MenuButton, MenuIconButton } from "../components/buttons";
+import { BackHandler, Platform, ToastAndroid, View } from 'react-native';
+import { Button, Text } from 'react-native-paper';
+import { MenuButton, MenuIconButton } from '../components/buttons';
+import { StatusBar      } from 'expo-status-bar';
 
 const RootPage = () => {
-    return(
-        <View style={{flex:1, justifyContent:'center', alignItems:'center'}}>
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
             <Text variant="displayLarge">AI Gimon</Text>
             {/* <MenuButton title="Play" route="/game" />
             <MenuButton title="Statistics" />
             <MenuButton title="Settings" route="/settings" /> */}
-            <View style={{flexDirection:'row', flexWrap:'wrap', justifyContent:'center'}}>
+            <View style={{ flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'center' }}>
                 <MenuIconButton icon="play" route="/game" />
-                <MenuIconButton icon="chart-bar" />
-                <MenuIconButton icon="cog" />
-                {Platform.OS === 'windows' || Platform.OS === 'macos' && <MenuIconButton icon="close" onPress={() => BackHandler.exitApp()} />}
+                <MenuIconButton icon="chart-bar" onPress={() => ToastAndroid.show('Stats coming soon', ToastAndroid.SHORT)} />
+                <MenuIconButton icon="cog" onPress={() => ToastAndroid.show('Working on it :)', ToastAndroid.SHORT)} />
+                {Platform.OS === 'windows' ||
+                    (Platform.OS === 'macos' && (
+                        <MenuIconButton icon="close" onPress={() => BackHandler.exitApp()} />
+                    ))}
             </View>
+            <StatusBar style={'light'} />
         </View>
     );
-}
+};
 
 export default RootPage;
