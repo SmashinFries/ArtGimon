c2a3bf6acf5799116cbf435889a1c42c480376ec
Smashinfries <jus10luka@gmail.com>
2023-10-29 17:28:25 -0500

    refactor

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,15 +1,30 @@
-import 'react-native-gesture-handler'
+import 'react-native-gesture-handler';
 import { Stack } from 'expo-router';
 import { MD3DarkTheme, MD3LightTheme, Provider, adaptNavigationTheme } from 'react-native-paper';
 import { DefaultTheme, DarkTheme as NavDarkTheme, ThemeProvider } from '@react-navigation/native';
+import { StatusBar } from 'expo-status-bar';
 
 const { LightTheme, DarkTheme } = adaptNavigationTheme({
-  reactNavigationLight: DefaultTheme,
-  reactNavigationDark: NavDarkTheme,
-  materialDark: MD3DarkTheme,
-  materialLight: MD3LightTheme,
+    reactNavigationLight: DefaultTheme,
+    reactNavigationDark: NavDarkTheme,
+    materialDark: MD3DarkTheme,
+    materialLight: MD3LightTheme,
 });
 
-export default function RootLayout() {
-  return <Provider theme={MD3DarkTheme}><ThemeProvider value={DarkTheme}><Stack screenOptions={{headerShown:false, animation:'slide_from_right', animationTypeForReplace:'push'}} /></ThemeProvider></Provider>;
-}
+const RootLayout = () => {
+    return (
+        <Provider theme={MD3DarkTheme}>
+            <ThemeProvider value={DarkTheme}>
+                <Stack
+                    screenOptions={{
+                        headerShown: false,
+                        animation: 'slide_from_right',
+                        animationTypeForReplace: 'push',
+                    }}
+                />
+            </ThemeProvider>
+        </Provider>
+    );
+};
+
+export default RootLayout;
