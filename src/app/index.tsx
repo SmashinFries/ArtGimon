967b274bc57fe3e7fe6cdcf79ba690b3d98d9f07
Smashinfries <jus10luka@gmail.com>
2023-11-02 00:37:08 -0500

    adjust status bar style

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,25 +1,28 @@
 import { BackHandler, Platform, ToastAndroid, View } from 'react-native';
-import { Button, Text } from 'react-native-paper';
-import { MenuButton, MenuIconButton } from '../components/buttons';
+import { Text } from 'react-native-paper';
+import { MenuIconButton } from '../components/buttons';
 import { StatusBar      } from 'expo-status-bar';
+import { useThemeStore } from '../store/themes';
 
 const RootPage = () => {
+    const { darkMode } = useThemeStore();
+
     return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text variant="displayLarge">AI Gimon</Text>
+            <Text variant="displayLarge">Art Gimon</Text>
             {/* <MenuButton title="Play" route="/game" />
             <MenuButton title="Statistics" />
             <MenuButton title="Settings" route="/settings" /> */}
             <View style={{ flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'center' }}>
                 <MenuIconButton icon="play" route="/game" />
                 <MenuIconButton icon="chart-bar" onPress={() => ToastAndroid.show('Stats coming soon', ToastAndroid.SHORT)} />
-                <MenuIconButton icon="cog" onPress={() => ToastAndroid.show('Working on it :)', ToastAndroid.SHORT)} />
+                <MenuIconButton icon="cog" route='/settings' />
                 {Platform.OS === 'windows' ||
                     (Platform.OS === 'macos' && (
                         <MenuIconButton icon="close" onPress={() => BackHandler.exitApp()} />
                     ))}
             </View>
-            <StatusBar style={'light'} />
+            <StatusBar style={darkMode ? 'light' : 'dark'} />
         </View>
     );
 };
